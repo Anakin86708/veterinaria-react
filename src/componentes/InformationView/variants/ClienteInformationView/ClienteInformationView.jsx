dd24fb2ae5938a34c1a7da518ba05063e9da2043
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-04-18 10:25:09 -0300

    Displaying success for clientes and especies.

--- src/componentes/InformationView/variants/ClienteInformationView/ClienteInformationView.jsx
+++ src/componentes/InformationView/variants/ClienteInformationView/ClienteInformationView.jsx
@@ -23,32 +23,44 @@ const ClienteInformationView = function (props) {
         setData({...data, "dataNascimento": dateConverterToService(e.target.value)})
     }
 
+    const formDisplay = (
+        <form className="gap-4 form-cliente">
+        <fieldset>
+            <label htmlFor="fNome">Nome</label>
+            <input type="text" name="nome" id="fNome" value={data.nome} onChange={e => setData({ ...data, "nome": e.target.value })} readOnly={currentState === "view"} />
+
+            <label htmlFor="fDataNascimento">Data de nascimento</label>
+            <input type="date" name="dataNascimento" id="fDataNascimento" value={dataNascimento} onChange={onChangeDataNascimento} readOnly={currentState === "view"} />
+
+            <label htmlFor="fEmail">Email</label>
+            <input type="email" name="email" id="fEmail" value={data.email} onChange={e => setData({ ...data, "email": e.target.value })} readOnly={currentState === "view"} />
+        </fieldset>
+
+        <fieldset>
+            <label htmlFor="fEndereco">Endereço</label>
+            <input type="text" name="endereco" id="fEndereco" value={data.endereco} onChange={e => setData({ ...data, "endereco": e.target.value })} readOnly={currentState === "view"} />
+
+            <label htmlFor="fTelefone">Telefone</label>
+            <input type="text" name="telefone" id="fTelefone" value={data.telefone} onChange={e => setData({ ...data, "telefone": e.target.value })} readOnly={currentState === "view"} />
+        </fieldset>
+        
+        {currentState === "add" || currentState === "edit" ?
+            <input className="btn btn-primary" type="button" onClick={saveData} value="Confirmar" onChange={() => { }} /> : null}
+        {currentState === "edit" ?
+            <input type="button" value="Cancelar" onClick={() => setCurrentState("view")} /> : null}
+    </form>
+    );
+
+    const successDisplay = (
+        <div className="success">
+            <h1>Sucesso!</h1>
+            <button className="btn btn-success" onClick={onClose}>Fechar</button>
+        </div>
+    );
+
     return (
         <InformationView showEdit={currentState === "view"} onClickEdit={() => setCurrentState("edit")} onClose={() => onClose}>
-            <form className="gap-4 form-cliente">
-                <fieldset>
-                    <label htmlFor="fNome">Nome</label>
-                    <input type="text" name="nome" id="fNome" value={data.nome} onChange={e => setData({ ...data, "nome": e.target.value })} readOnly={currentState === "view"} />
-
-                    <label htmlFor="fDataNascimento">Data de nascimento</label>
-                    <input type="date" name="dataNascimento" id="fDataNascimento" value={dataNascimento} onChange={onChangeDataNascimento} readOnly={currentState === "view"} />
-
-                    <label htmlFor="fEmail">Email</label>
-                    <input type="email" name="email" id="fEmail" value={data.email} onChange={e => setData({ ...data, "email": e.target.value })} readOnly={currentState === "view"} />
-                </fieldset>
-
-                <fieldset>
-                    <label htmlFor="fEndereco">Endereço</label>
-                    <input type="text" name="endereco" id="fEndereco" value={data.endereco} onChange={e => setData({ ...data, "endereco": e.target.value })} readOnly={currentState === "view"} />
-
-                    <label htmlFor="fTelefone">Telefone</label>
-                    <input type="text" name="telefone" id="fTelefone" value={data.telefone} onChange={e => setData({ ...data, "telefone": e.target.value })} readOnly={currentState === "view"} />
-                </fieldset>
-                {currentState === "add" || currentState === "edit" ?
-                    <input className="btn btn-primary" type="button" onClick={saveData} value="Confirmar" onChange={() => { }} /> : null}
-                {currentState === "edit" ?
-                    <input type="button" value="Cancelar" onClick={() => setCurrentState("view")} /> : null}
-            </form>
+            {currentState !== "saved" ? formDisplay : successDisplay}
         </InformationView>
     );
 }
--- src/componentes/InformationView/variants/EspeciesInformationView/EspeciesInformationView.jsx
+++ src/componentes/InformationView/variants/EspeciesInformationView/EspeciesInformationView.jsx
@@ -4,14 +4,18 @@ import InformationView from "../../InformationView";
 
 const EspeciesInformationView = function (props) {
     const onClose = props.onClose;
-    
+
     const [data, setData] = useState(props.data);
     const [currentState, setCurrentState] = useState("id" in data ? "view" : "add");
 
     const saveData = function () {
         if (currentState === "add") {
             insertNewEspecie(data)
-            .then(r => console.log(r.status));
+                .then(r => {
+                    if (r.ok) {
+                        setCurrentState("saved");
+                    }
+                });
         }
     }
 
@@ -20,18 +24,28 @@ const EspeciesInformationView = function (props) {
         console.log("Show edit: " + currentState === "view");
     }, [currentState]);
 
+    const formDisplay = (
+        <form className="gap-4 form-animais">
+            <fieldset>
+                <label htmlFor="fNome">Nome</label>
+                <input type="text" name="nome" id="fNome" value={data?.nome} onChange={e => { setData({ ...data, "nome": e.target.value }) }} readOnly={currentState === "view"} />
+            </fieldset>
+            {currentState === "add" || currentState === "edit" ?
+                <input className="btn btn-primary" type="button" onClick={saveData} value="Confirmar" onChange={() => { }} /> : null}
+            {currentState === "edit" ?
+                <input type="button" value="Cancelar" onClick={() => setCurrentState("view")} /> : null}
+        </form>
+    );
+
+    const displaySuccess = (
+        <div className="success">
+            <h1>Sucesso!</h1>
+            <button className="btn btn-success" onClick={onClose}>Fechar</button>
+        </div>
+    );
     return (
         <InformationView showEdit={currentState === "view"} onClickEdit={() => setCurrentState("edit")} onClose={() => onClose}>
-            <form className="gap-4 form-animais">
-                <fieldset>
-                    <label htmlFor="fNome">Nome</label>
-                    <input type="text" name="nome" id="fNome" value={data?.nome} onChange={e => {setData({...data, "nome": e.target.value})}} readOnly={currentState === "view"} />
-                </fieldset>
-                {currentState === "add" || currentState === "edit" ?
-                    <input className="btn btn-primary" type="button" onClick={saveData} value="Confirmar" onChange={() => {}}/> : null}
-                { currentState === "edit" ?
-                <input type="button" value="Cancelar" onClick={() => setCurrentState("view")} />    : null}
-            </form>
+            {currentState !== "saved" ? formDisplay : displaySuccess}
         </InformationView>
     );
 }
